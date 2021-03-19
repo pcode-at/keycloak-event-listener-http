2a9b7cd6dea37e9887503f682339b1f4f4670f2e
Amir Khasanov <aworldx@gmail.com>
2021-03-19 14:16:59 +0300

    fixes

--- src/main/java/org/softwarefactory/keycloak/providers/events/http/HTTPEventListenerProvider.java
+++ src/main/java/org/softwarefactory/keycloak/providers/events/http/HTTPEventListenerProvider.java
@@ -23,7 +23,13 @@ import org.keycloak.events.EventType;
 import org.keycloak.events.admin.AdminEvent;
 import org.keycloak.events.admin.OperationType;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONString;
+
 import java.util.Map;
+import java.util.HashMap;
 import java.util.Set;
 import java.lang.Exception;
 
@@ -44,6 +50,7 @@ public class HTTPEventListenerProvider implements EventListenerProvider {
     private String password;
     public static final String publisherId = "keycloak";
     public String TOPIC;
+    public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
 
     public HTTPEventListenerProvider(Set<EventType> excludedEvents, Set<OperationType> excludedAdminOperations, String serverUri, String username, String password, String topic) {
         this.excludedEvents = excludedEvents;
@@ -62,24 +69,26 @@ public class HTTPEventListenerProvider implements EventListenerProvider {
         } else {
             String stringEvent = toString(event);
             try {
-            	RequestBody formBody = new FormBody.Builder()
-                        .add("json", stringEvent)
-                        .build();
+                RequestBody formBody = RequestBody.create(JSON, stringEvent);
+
+                // RequestBody formBody = new FormBody.Builder()
+                // .add("json", stringEvent)
+                // .build();
 
                 okhttp3.Request.Builder builder = new Request.Builder()
                         .url(this.serverUri)
                         .addHeader("User-Agent", "KeycloakHttp Bot");
-            	
+
 
                 if (this.username != null && this.password != null) {
                 	builder.addHeader("Authorization", "Basic " + this.username + ":" + this.password.toCharArray());
                 }
-                
+
                 Request request = builder.post(formBody)
                         .build();
-                
+
             	Response response = httpClient.newCall(request).execute();
-            	
+
             	if (!response.isSuccessful()) {
             		throw new IOException("Unexpected code " + response);
             	}
@@ -103,24 +112,26 @@ public class HTTPEventListenerProvider implements EventListenerProvider {
         } else {
             String stringEvent = toString(event);
             try {
-            	RequestBody formBody = new FormBody.Builder()
-                        .add("json", stringEvent)
-                        .build();
+                RequestBody formBody = RequestBody.create(JSON, stringEvent);
+
+                // RequestBody formBody = new FormBody.Builder()
+                // .add("json", stringEvent)
+                // .build();
 
                 okhttp3.Request.Builder builder = new Request.Builder()
                         .url(this.serverUri)
                         .addHeader("User-Agent", "KeycloakHttp Bot");
-            	
+
 
                 if (this.username != null && this.password != null) {
                 	builder.addHeader("Authorization", "Basic " + this.username + ":" + this.password.toCharArray());
                 }
-                
+
                 Request request = builder.post(formBody)
                         .build();
-                
+
             	Response response = httpClient.newCall(request).execute();
-            	
+
             	if (!response.isSuccessful()) {
             		throw new IOException("Unexpected code " + response);
             	}
@@ -138,65 +149,47 @@ public class HTTPEventListenerProvider implements EventListenerProvider {
 
 
     private String toString(Event event) {
-        StringBuilder sb = new StringBuilder();
+        JSONObject json = new JSONObject();
+        json.put("type", event.getType());
+        json.put("realm_id", event.getRealmId());
+        json.put("client_id", event.getClientId());
 
-        sb.append("{'type': '");
-        sb.append(event.getType());
-        sb.append("', 'realmId': '");
-        sb.append(event.getRealmId());
-        sb.append("', 'clientId': '");
-        sb.append(event.getClientId());
-        sb.append("', 'userId': '");
-        sb.append(event.getUserId());
-        sb.append("', 'ipAddress': '");
-        sb.append(event.getIpAddress());
-        sb.append("'");
+        json.put("user_id", event.getUserId());
+        json.put("ip_address", event.getIpAddress());
+        json.put("clientId", event.getClientId());
 
         if (event.getError() != null) {
-            sb.append(", 'error': '");
-            sb.append(event.getError());
-            sb.append("'");
+            json.put("error", event.getError());
         }
-        sb.append(", 'details': {");
+
+        JSONObject details = new JSONObject();
+
         if (event.getDetails() != null) {
             for (Map.Entry<String, String> e : event.getDetails().entrySet()) {
-                sb.append("'");
-                sb.append(e.getKey());
-                sb.append("': '");
-                sb.append(e.getValue());
-                sb.append("', ");
+                details.put(e.getKey(), e.getValue());
             }
-        sb.append("}}");
         }
 
-        return sb.toString();
-    }
-    
-    
-    private String toString(AdminEvent adminEvent) {
-        StringBuilder sb = new StringBuilder();
+        json.put("details", details);
 
-        sb.append("{'type': '");
-        sb.append(adminEvent.getOperationType());
-        sb.append("', 'realmId': '");
-        sb.append(adminEvent.getAuthDetails().getRealmId());
-        sb.append("', 'clientId': '");
-        sb.append(adminEvent.getAuthDetails().getClientId());
-        sb.append("', 'userId': '");
-        sb.append(adminEvent.getAuthDetails().getUserId());
-        sb.append("', 'ipAddress': '");
-        sb.append(adminEvent.getAuthDetails().getIpAddress());
-        sb.append("', 'resourcePath': '");
-        sb.append(adminEvent.getResourcePath());
-        sb.append("'");
+        return json.toString(2);
+    }
+
+    private String toString(AdminEvent adminEvent) {
+        JSONObject json = new JSONObject();
+        json.put("type", adminEvent.getOperationType());
+        json.put("realm_id", adminEvent.getAuthDetails().getRealmId());
+        json.put("client_id", adminEvent.getAuthDetails().getClientId());
+
+        json.put("user_id", adminEvent.getAuthDetails().getUserId());
+        json.put("ip_address", adminEvent.getAuthDetails().getIpAddress());
+        json.put("resource_path", adminEvent.getResourcePath());
 
         if (adminEvent.getError() != null) {
-            sb.append(", 'error': '");
-            sb.append(adminEvent.getError());
-            sb.append("'");
+            json.put("error", adminEvent.getError());
         }
-        sb.append("}");
-        return sb.toString();
+
+        return json.toString(2);
     }
 
     @Override
